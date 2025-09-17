f0e6a9e77cfc8f2375a90bfd03a5a79084432e73
phntoahjhb024 <phntoahjhb024@student.wethinkcode.co.za>
2025-09-17 15:31:46 +0200

    Initialized Server Bootstrap

--- src/main/java/Broadcast/Server/BroadcastServer.java
+++ src/main/java/Broadcast/Server/BroadcastServer.java
@@ -1,4 +1,13 @@
 package Broadcast.Server;
 
+import org.glassfish.tyrus.server.Server;
+
 public class BroadcastServer {
+    public static void main(String[] args){
+        Server server = new Server(BroadcastEndpoint.class, "localhost", 8080, "/ws");
+
+        try{
+            server.start();
+        }
+    }
 }
